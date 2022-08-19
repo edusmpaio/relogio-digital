5fa78c270c8b484ca8d1c0b1e5bc69e824ba6aad
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-08-19 00:27:29 -0300

    Ajeitando erros nos if's

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,15 @@ const clock = setInterval(function time() {
   let minutes = dateToday.getMinutes();
   let seconds = dateToday.getSeconds();
 
-  if (hours < 10) return (hours = "0" + hours);
+  if (hours < 10) hours = "0" + hours;
 
-  if (minutes < 10) return (minutes = "0" + minutes);
+  if (minutes < 10) minutes = "0" + minutes;
 
-  if (seconds < 10) return (seconds = "0" + seconds);
+  if (seconds < 10) seconds = "0" + seconds;
 
   documentHours.textContent = hours;
   documentMinutes.textContent = minutes;
   documentSeconds.textContent = seconds;
 });
+
+console.log("teste");
